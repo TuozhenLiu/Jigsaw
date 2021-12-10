282565d8761cf757e1d91bf3b40b8a3c44d46fdb
TZ <tuozhen_liu@163.com>
2021-12-10 13:51:17 +0800

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -17,3 +17,36 @@ python ../input/jigsawscript/main.py \
   --log_interval 100 \
   --eval_interval 1000 \
   --eval_iters 100
+
+python ../input/jigsawscript/main.py \
+  --train_path ../input/jigsaw-toxic-comment-classification-challenge \
+  --valid_path ../input/jigsaw-toxic-severity-rating/validation_data.csv \
+  --modified_train_path xxx \
+  --model_path ../input/roberta-transformers-pytorch/roberta-large \
+  --checkpoint_path ./checkpoint \
+  --seed 2823 \
+  --fold 0 \
+  --n_epochs 1 \
+  --pos_neg_pairs 10 \
+  --train_batch_size 16 \
+  --valid_batch_size 32 \
+  --n_accumulate 4 \
+  --lr 3e-4 \
+  --min_lr 1e-5 \
+  --T_max 3000 \
+  --log_interval 10 \
+  --eval_interval 1000 \
+  --eval_iters 100 \
+  --hidden_size 1024
+
+python ../input/jigsawscript/eval.py \
+  --model_path ../input/roberta-transformers-pytorch/roberta-base \
+  --valid_path ../input/jigsaw-toxic-severity-rating/comments_to_score.csv \
+  --eval_model ../input/robertabase3000iters/checkpoint/fold-0-epoch0-1000.bin \
+               ../input/robertabase3000iters/checkpoint/fold-0-epoch0-2000.bin \
+               ../input/robertabase3000iters/checkpoint/fold-0-epoch0-3000.bin \
+               ../input/robertabase3000iters/checkpoint/fold-1-epoch0-1000.bin \
+               ../input/robertabase3000iters/checkpoint/fold-1-epoch0-2000.bin \
+               ../input/robertabase3000iters/checkpoint/fold-1-epoch0-3000.bin \
+  --seed 1234 \
+  --valid_batch_size 128
