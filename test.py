20a9eccd3daf87cf97dfd34ab67389323ad7e77b
TZ <tuozhen_liu@163.com>
2022-03-12 15:40:07 +0800

    Update

--- test.py
+++ test.py
@@ -4,40 +4,31 @@
 # Date: 2021/12/9
 # Description:
 # ----------------------------------------------------------------------------------------------------------------------
-import torch
-from torch.utils.data import Dataset, DataLoader
-
-
-class test_dataset(Dataset):
+class args:
     def __init__(self):
-        self.x = list(range(10))
-
-    def __len__(self):
-        return len(self.x)
-
-    def __getitem__(self, index):
-        return self.x[index]
-
-
-torch.random.manual_seed(1234)
-test_dataloader = DataLoader(
-        test_dataset(),
-        batch_size=1,
-        shuffle=True,
-        pin_memory=True,
-        drop_last=True
-)  # sampler
-
-i = 0
-for data in test_dataloader:
-    print(data)
-    i = i + 1
-    if i == 3:
-        break
-
-i = 0
-for data in test_dataloader:
-    print(data)
-    i = i + 1
-    if i == 3:
-        break
+        self.train_path = '../input/jigsaw-toxic-severity-rating/validation_data.csv'
+        self.modified_train_path = '../input/jigsawmodify-train-data/modified_train.csv'
+        self.valid_path = '../input/jigsaw-toxic-severity-rating/validation_data.csv'
+        self.eval_model = []
+        self.model_path = '../input/jigsaw-toxic-severity-rating/validation_data.csv'
+        self.checkpoint_path = '../input/jigsaw-toxic-severity-rating/validation_data.csv'
+        self.pos_neg_pairs = 10,
+        self.n_folds = 10,
+        self.fold = []
+        self.seed = 1234,
+        self.max_length = 128,
+        self.n_epochs = 3
+        self.train_batch_size = 32
+        self.valid_batch_size = 64,
+        self.n_accumulate = 1,
+        self.log_interval = 100,
+        self.save_interval = 1000,
+        self.eval_interval = 1000,
+        self.eval_iters = 10,
+        self.margin = 0.5,
+        self.dropout = 0.2,
+        self.hidden_size = 768,
+        self.lr = 1e-4,
+        self.T_max = 3000,
+        self.min_lr = 1e-6,
+        self.weight_decay = 1e-6
